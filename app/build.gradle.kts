2ea986ba9edd523460a4cf82561d7a754e04a320
Saad <reach.saad@outlook.com>
2025-09-13 16:51:20 +0530

    Saad

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
+
 }
 
 android {
@@ -26,6 +27,16 @@ android {
             )
         }
     }
+
+    buildFeatures {
+        viewBinding = true
+        compose = false
+    }
+
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.5.1"
+    }
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
@@ -33,13 +44,9 @@ android {
     kotlinOptions {
         jvmTarget = "11"
     }
-    buildFeatures {
-        viewBinding = true
-    }
 }
 
 dependencies {
-
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
@@ -49,4 +56,28 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+
+    implementation("androidx.compose.ui:ui")
+    implementation("androidx.compose.material3:material3")
+    implementation("androidx.navigation:navigation-compose")
+    implementation("androidx.datastore:datastore-preferences")
+    implementation("androidx.core:core-ktx")
+    implementation("androidx.compose.ui:ui-text-google-fonts")
+
+
+
+    // Compose BOM (keeps versions in sync)
+    implementation(platform("androidx.compose:compose-bom:2024.09.00"))
+
+    // Core Compose libraries
+    implementation("androidx.compose.ui:ui")
+    implementation("androidx.compose.material3:material3")
+    implementation("androidx.compose.ui:ui-tooling-preview")
+    debugImplementation("androidx.compose.ui:ui-tooling")
+
+    // Navigation for Compose
+    implementation("androidx.navigation:navigation-compose:2.8.0")
+
+    // Animations
+    implementation("androidx.compose.animation:animation")
 }
