7a824d9580ed7cf631489d742ccd6ea7c026dba0
saad2134 <reach.saad@outlook.com>
2026-09-12 15:32:29 +0530

    build: Enable automatic APK signing on release build type

--- patient/android/app/build.gradle.kts
+++ patient/android/app/build.gradle.kts
@@ -28,6 +28,7 @@ android {
         release {
             isMinifyEnabled = false
             isShrinkResources = false
+            signingConfig = signingConfigs.getByName("debug")
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
