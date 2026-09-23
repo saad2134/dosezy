f107cc9677a547d5ece078554c8546ca224d9614
saad2134 <reach.saad@outlook.com>
2026-09-23 20:56:42 +0530

    v2.5.6: Init

--- patient/android/app/build.gradle.kts
+++ patient/android/app/build.gradle.kts
@@ -22,8 +22,8 @@ android {
         applicationId = "com.saad2134.dosezy"
         minSdk = 24
         targetSdk = 36
-        versionCode = 30
-        versionName = "2.5.5"
+        versionCode = 31
+        versionName = "2.5.6"
         buildConfigField("String", "VERSION_NAME", "\"$versionName\"")
         buildConfigField("int", "VERSION_CODE", "$versionCode")
 
